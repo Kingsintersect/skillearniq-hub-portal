d0190cd396f6ff9f39650f996f728f83b28e3630
Kingsintersect <kingsintersect@gmail.com>
2025-09-24 09:56:28 +0100

    Role ui structure setup

---
+++ src/app/(dashboard)/manager/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ManagerDashboardPage = () => {
+  return (
+    <div>ManagerDashboardPage</div>
+  )
+}
---
+++ src/app/(dashboard)/teacher/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TeacherDashboardPage = () => {
+  return (
+    <div>TeacherDashboardPage</div>
+  )
+}
