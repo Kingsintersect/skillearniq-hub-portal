7cf1b8ff33826d54cd19ad10b354a9eb0d293e08
Kingsintersect <kingsintersect@gmail.com>
2025-12-06 02:30:15 +0100

    fixing deployment error-2

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,28 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Add this for now to ensure deployment
   },
+
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
